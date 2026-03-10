ab7059c7eecb68343eefe9f77e39bbc0d79855aa
DevarathN <devarathnediyirippil@gmail.com>
2026-03-10 23:02:25 +0530

    Update auth.js

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-const API = "http://localhost:5000/api/auth";
-
-export const signup = async (
-  name,
-  phoneNumber,
-  email,
-  password,
-  companyName,
-) => {
-  const res = await fetch(`${API}/signup`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ name, phoneNumber, email, password, companyName }),
-  });
-  return res.json();
-};
-
-export const login = async (email, password) => {
-  const res = await fetch(`${API}/login`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, password }),
-  });
-  return res.json();
-};
-export const getProfile = async () => {
-  const token = localStorage.getItem("token");
-
-  const res = await fetch(`${API}/me`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  return res.json();
-};
-=======
 const API = import.meta.env.VITE_API_URL;
 
 export const signup = async (
@@ -72,4 +34,4 @@ export const getProfile = async () => {
 
   return res.json();
 };
->>>>>>> ec606bc0b8c29c88b9511a1cc2d2baf30b8e2673
+
